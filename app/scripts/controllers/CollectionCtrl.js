fc1c2ae2c82fba9c4cf58f0d1b86af23e9c83303
Nick Cheung <nickhcheung@hotmail.com>
2017-03-17 00:29:41 -0700

    Checkpoint 6 Services Pt. 1: completed

--- app/scripts/controllers/CollectionCtrl.js
+++ app/scripts/controllers/CollectionCtrl.js
@@ -1,12 +1,13 @@
 (function(){
-  function CollectionCtrl(){
+  function CollectionCtrl(Fixtures){
     this.albums = [];
 
-    this.albums.push(angular.copy(albumPicasso));
-    this.albums.push(angular.copy(wakingAtDawn));
+    this.albums.push(Fixtures.getAlbum());
+    this.albums.push(Fixtures.getSecondAlbum());
   }
 
   angular
     .module("blocJams")
-    .controller("CollectionCtrl", CollectionCtrl);
+    .controller("CollectionCtrl", ["Fixtures", CollectionCtrl]);
+
 })();
